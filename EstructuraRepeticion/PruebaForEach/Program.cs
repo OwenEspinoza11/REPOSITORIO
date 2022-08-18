79cc9ba26a0e80d3fca35043519cda26e5617e0b
OwenEspinoza11 <110624917+OwenEspinoza11@users.noreply.github.com>
2022-08-18 12:13:28 -0600

    Add files via upload
    dfgdfgdfg

---
+++ EstructuraRepeticion/PruebaForEach/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] valores;
+valores = new int[] {3,5,6,5,8,9,1,5,5};
+string[] paises= {"Nicaragua","Cuba","Venezuela"};
+
+foreach (int i in valores)
+{
+    total += i;
+    Console.WriteLine("{0} ", i);
+}
+Console.WriteLine("Total: " + total);
+int[,] numeros = new int[2, 2] { { 5, 2 }, { 2, 3 } };
+numeros [1, 1] =10
+
+for each(int i in numeros)
+{
+    Console.WriteLine("{0} ", i);
+}
+
+Console.WriteLine(valores.Length);
+
+int[][] matriz = new int[3][];
